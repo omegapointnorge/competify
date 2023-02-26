cd826e42997c55cdbf09a6af38e3b60e266335bd
Brede Fritjof Klausen <brede@fritjof.no>
2023-02-26 23:37:56 +0100

    Ad key vault

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,8 +1,12 @@
 using Data;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 
